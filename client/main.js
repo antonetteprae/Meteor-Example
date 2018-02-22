2c137e97721ea40fd2b6bb7ea65e2cafcef59e17
prae <antonetteprae@gmail.com>
2018-02-22 19:27:23 +0700

    add graph and bootstrap

--- client/main.js
+++ client/main.js
@@ -2,16 +2,44 @@
 var count = 0
 Router.route('/', function () {
   // render the Home template with a custom data context
-  var xxx = 'nnn'
-  let userAgent = window.navigator.userAgent
+  //let userAgent = window.navigator.userAgent
+  var browser_name = '';
+  isIE = /*@cc_on!@*/false || !!document.documentMode;
+  isEdge = !isIE && !!window.StyleMedia;
+  if(navigator.userAgent.indexOf("Chrome") != -1 && !isEdge)
+  {
+      browser_name = 'chrome';
+  }
+  else if(navigator.userAgent.indexOf("Safari") != -1 && !isEdge)
+  {
+      browser_name = 'safari';
+  }
+  else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+  {
+      browser_name = 'firefox';
+  }
+  else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+  {
+      browser_name = 'ie';
+  }
+  else if(isEdge)
+  {
+      browser_name = 'edge';
+  }
+  else 
+  {
+     browser_name = 'other-browser';
+  }
+  $('html').addClass(browser_name);
   count++;
-  this.render('Home', {data: {title: count}});
+  this.render('Home', {data: {title: count, browser: browser_name}});
 });
 
-// when you navigate to "/one" automatically render the template named "One".
-Router.route('/one', function() {
- this.render('One', {data: {example: count}})	
+// navigate to "/details" automatically render the template named "Details".
+Router.route('/details', function() {
+ this.render('Details', {data: {example: count}})	
 });
 
-// when you navigate to "/two" automatically render the template named "Two".
-Router.route('/two');
+
+
+

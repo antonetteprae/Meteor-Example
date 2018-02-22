8357f8e26cf0530e99f29559a25c9c34591610b5
prae <antonetteprae@gmail.com>
2018-02-22 21:29:30 +0700

    modified format

--- imports/browser.js
+++ imports/browser.js
@@ -3,28 +3,17 @@ export const getBrowserName = function(){
 var browser_name = '';
   isIE = /*@cc_on!@*/false || !!document.documentMode;
   isEdge = !isIE && !!window.StyleMedia;
-  if(navigator.userAgent.indexOf("Chrome") != -1 && !isEdge)
-  {
+  if(navigator.userAgent.indexOf("Chrome") != -1 && !isEdge){
       browser_name = 'chrome';
-  }
-  else if(navigator.userAgent.indexOf("Safari") != -1 && !isEdge)
-  {
+  }else if(navigator.userAgent.indexOf("Safari") != -1 && !isEdge){
       browser_name = 'safari';
-  }
-  else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
-  {
+  }else if(navigator.userAgent.indexOf("Firefox") != -1 ) {
       browser_name = 'firefox';
-  }
-  else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
-  {
+  }else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) {
       browser_name = 'ie';
-  }
-  else if(isEdge)
-  {
+  }else if(isEdge){
       browser_name = 'edge';
-  }
-  else 
-  {
+  }else {
      browser_name = 'other-browser';
   }
   return browser_name

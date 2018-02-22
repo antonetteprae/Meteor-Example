aa7df50fd9e739d8f284d1d6be313307c4e8a1ad
prae <antonetteprae@gmail.com>
2018-02-22 21:23:26 +0700

    fetch data and display

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@ import { Tasks } from '../imports/api/tasks.js';
 import { getBrowserName } from '../imports/browser.js'
 import Highcharts from 'highcharts';
 
-var count = 0
 Router.route('/', function () {
   // render the Home template with a custom data context
   const browser_name = getBrowserName()
@@ -45,7 +44,14 @@ if (Meteor.isClient) {
 
 // navigate to "/details" automatically render the template named "Details".
 Router.route('/details', function() {
-    //let userAgent = window.navigator.userAgent
+  
+  Template.Details.helpers({
+    browsers: () => {
+      return Tasks.find({}).fetch();
+    },
+  });
+
+  
   const browser_name = getBrowserName();
   $('html').addClass(browser_name);
  this.render('Details', {data: {browser: browser_name}})	

9aff6838058acd0f7c79fd9eb8db399214a008a5
prae <antonetteprae@gmail.com>
2018-02-22 20:46:02 +0700

    change import highcharts static to npm move highcharts into main

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 
 import { Tasks } from '../imports/api/tasks.js';
 import { getBrowserName } from '../imports/browser.js'
+import Highcharts from 'highcharts';
 
 var count = 0
 Router.route('/', function () {
@@ -8,6 +9,31 @@ Router.route('/', function () {
 //   const data = Tasks.find({}).count()
 //   Tasks.insert({ browser: ""});
 //   console.log('>>', JSON.stringify(data));
+if (Meteor.isClient) {
+      Meteor.defer(function() {
+        // Create standard Highcharts chart with options:
+        Highcharts.chart('mychart', {
+          chart: {
+              type: 'bar'
+          }, 
+          title: {
+              text: 'Browser'
+          },
+          xAxis: {
+              categories: ['Chrome', 'Safari', 'Firefox','IE','Edge','Others']
+          },
+          yAxis: {
+              title: {
+                  text: 'Total'
+              }
+          },
+          series: [{
+              name: 'Client',
+              data: [1, 0, 4, 3, 3, 3]
+          }]
+        });
+      });
+}
   this.render('Home');
 });
 
